833759e72c479fdd84732f0be0d3029dcd27bb1d
1LINCHI1 <linchein1@gmail.com>
2025-10-29 11:08:35 -0700

    test

---
+++ test.py
@@ -0,0 +1,15 @@
+def main():
+	print("example")
+	print("example1")
+	print("example2")
+	print("example3")
+	print("example4")
+
+	while True:
+		command = input(">>>").strit().lower()
+
+		if command == "exit":
+			break
+
+if __name__ == "__main__":
+	main()
