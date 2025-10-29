83c49ccc53cc46dc6dd68024978edfadc2510e5a
1LINCHI1 <linchein1@gmail.com>
2025-10-29 12:05:21 -0700

    added Nmap

--- test.py
+++ test.py
@@ -6,11 +6,23 @@ def main():
 	print("example4")
 
 	while True:
-		command = input(">>>").strit().lower()
+		command = input(">>>").strip().lower()
 
 		if command == "exit":
 			print("bye")
 			break
+		elif command == "a":
+			import nmap
+			print("nmap is ready")
+			host = input("host:")
+			nm = nmap.PortScanner()
+			nm.scan(host, '22-443')
+			nm.command_line()
+			f'nmap -oX - -p 22-443 -sV {host}'
+			nm.scaninfo()
+
+
+
 
 if __name__ == "__main__":
 	main()
